89907a7a2af6bb6b5e52d03b9033fa3691bd81af
Frank-Colorado <chawnkiee@gmail.com>
2023-07-16 21:14:26 -0400

    imported dependencies in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,19 @@
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
 const App = () => {
   return (
     <div>
