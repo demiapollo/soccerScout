1c97696a30b2906f7cf2faa482de08e7f419f2ed
Deshan <dcnm777@gmail.com>
2023-07-17 17:55:48 -0700

    more navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,84 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, TextField, Button, IconButton, Popover, List, ListItem, ListItemText, Menu, MenuItem } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
-const Navbar = () => {
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            My App
-          </Typography>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">About</Button>
-          <Button color="inherit">Contact</Button>
-          <Button color="inherit">Dashboard</Button>
-        </Toolbar>
-      </AppBar>
-    );
+const NavBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [filterAnchorEl, setFilterAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  export default Navbar;
+  const handleFilterClick = (event) => {
+    setFilterAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleFilterClose = () => {
+    setFilterAnchorEl(null);
+  };
+
+  const filterOpen = Boolean(filterAnchorEl);
+  const id = filterOpen ? 'simple-popover' : undefined;
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h3" style={{ flexGrow: 1 }}>
+          Soccer Scout
+        </Typography>
+        <IconButton color="inherit" onClick={handleFilterClick}>
+          <MenuIcon />
+        </IconButton>
+        <Popover
+          id={id}
+          open={filterOpen}
+          anchorEl={filterAnchorEl}
+          onClose={handleFilterClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          <List>
+            <ListItem button>
+              <ListItemText primary="Option 1" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Option 2" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Option 3" />
+            </ListItem>
+          </List>
+        </Popover>
+        <TextField id="input-with-icon-textfield" />
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Dashboard</Button>
+        <IconButton color="inherit" onClick={handleClick}>
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>Sign Out</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
