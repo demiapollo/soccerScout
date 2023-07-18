e731d07eb3f2bcd448bbe283bb7d5509493b59c0
demiapollo <apollodemirel67@gmail.com>
2023-07-17 19:53:04 -0700

    bcrypt added to userProfile

--- server/models/UserProfile.js
+++ server/models/UserProfile.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   name: {
@@ -18,6 +19,21 @@ const UserSchema = new Schema({
     minlength: 5,
   },
 });
+
+// set up pre-save middleware to create password
+profileSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // compare the incoming password with the hashed password
+  profileSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
 
 const User = model("User", UserSchema);
 
