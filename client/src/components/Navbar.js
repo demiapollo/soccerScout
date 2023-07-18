69268fa65d36de03e755d55b45b9f80abfc5c060
Deshan <dcnm777@gmail.com>
2023-07-18 16:12:48 -0700

    navbar search tab functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, TextField, Button, IconButton, Popover, List, ListItem, ListItemText, Menu, MenuItem } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, TextField, Button, IconButton, Popover, List, ListItem, ListItemText, Menu, MenuItem, Tabs, Tab } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const Navbar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [filterAnchorEl, setFilterAnchorEl] = useState(null);
+  const [value, setValue] = React.useState(0);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -26,58 +27,58 @@ const Navbar = () => {
   const filterOpen = Boolean(filterAnchorEl);
   const id = filterOpen ? 'simple-popover' : undefined;
 
+  const handleTabChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h3" style={{ flexGrow: 1 }}>
-          Soccer Scout
-        </Typography>
-        <IconButton color="inherit" onClick={handleFilterClick}>
-          <MenuIcon />
-        </IconButton>
-        <Popover
-          id={id}
-          open={filterOpen}
-          anchorEl={filterAnchorEl}
-          onClose={handleFilterClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-        >
-          <List>
-            <ListItem button>
-              <ListItemText primary="Option 1" />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="Option 2" />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary="Option 3" />
-            </ListItem>
-          </List>
-        </Popover>
-        <TextField id="input-with-icon-textfield" />
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">Dashboard</Button>
-        <IconButton color="inherit" onClick={handleClick}>
-          <AccountCircle />
-        </IconButton>
-        <Menu
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>Sign Out</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h3" style={{ flexGrow: 1 }}>
+            Soccer Scout
+          </Typography>
+          <IconButton color="inherit" onClick={handleFilterClick}>
+            <MenuIcon />
+          </IconButton>
+          <Popover
+            id={id}
+            open={filterOpen}
+            anchorEl={filterAnchorEl}
+            onClose={handleFilterClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <Tabs value={value} onChange={handleTabChange} aria-label="simple tabs example">
+              <Tab label="Countries" />
+              <Tab label="Teams" />
+              <Tab label="Leagues" />
+            </Tabs>
+          </Popover>
+          <TextField id="input-with-icon-textfield" />
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Dashboard</Button>
+          <IconButton color="inherit" onClick={handleClick}>
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>Sign Out</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
