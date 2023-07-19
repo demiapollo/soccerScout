d199ec8baf8dbc873e78a9e0123acbd17f5630af
Frank-Colorado <chawnkiee@gmail.com>
2023-07-19 18:32:23 -0400

    created stringToColor helper function

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,54 +1,14 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  Grid,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-} from "@material-ui/core";
-import PersonIcon from "@material-ui/icons/Person";
-import FolderIcon from "@material-ui/icons/Folder";
-import DeleteIcon from "@material-ui/icons/Delete";
+import { Grid, Typography } from "@material-ui/core";
+
+import { stringAvatar } from "../utils/helpers";
 
 import Avatar from "@material-ui/core/Avatar";
 import { Navigate, Link, useParams } from "react-router-dom";
 
 import UserTab from "../components/UserTab";
-
-const stringToColor = (string) => {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = "#";
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  console.log(color);
-
-  return color;
-};
-
-const stringAvatar = (name) => {
-  const here = {
-    style: {
-      backgroundColor: stringToColor(name),
-    },
-    children: `${name.split("")[0]}${name.split("")[1]}`,
-  };
-  console.log(here);
-  return here;
-};
 
 const Profile = () => {
   const useStyles = makeStyles((theme) => ({
