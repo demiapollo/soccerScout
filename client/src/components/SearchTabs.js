5afe0e2095417d299ca774a98245d4dfe4132fd6
Deshan <dcnm777@gmail.com>
2023-07-18 19:06:20 -0700

    switched search tabs from horizontal to vertical

--- client/src/components/SearchTabs.js
+++ client/src/components/SearchTabs.js
@@ -1,7 +1,7 @@
 // SearchTabs.js
 
 import React from 'react';
-import { Tabs, Tab, Popover } from '@material-ui/core';
+import { Tabs, Tab, Popover, Box } from '@material-ui/core';
 
 const SearchTabs = ({ filterOpen, handleFilterClose, filterAnchorEl, handleTabChange, value }) => {
     const id = filterOpen ? 'simple-popover' : undefined;
@@ -21,11 +21,18 @@ const SearchTabs = ({ filterOpen, handleFilterClose, filterAnchorEl, handleTabCh
               horizontal: 'center',
             }}
         >
-            <Tabs value={value} onChange={handleTabChange} aria-label="simple tabs example">
-                <Tab label="Players" />
-                <Tab label="Teams" />
-                <Tab label="Leagues" />
-            </Tabs>
+            <Box style={{ minWidth: 150 }}>  {/* You may need to adjust minWidth based on your needs */}
+              <Tabs 
+                value={value} 
+                onChange={handleTabChange} 
+                aria-label="simple tabs example"
+                orientation="vertical"
+              >
+                  <Tab label="Players" />
+                  <Tab label="Teams" />
+                  <Tab label="Leagues" />
+              </Tabs>
+            </Box>
         </Popover>
     );
 };
