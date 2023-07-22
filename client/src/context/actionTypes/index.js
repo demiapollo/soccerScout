e681544ab0625d7deee7516179e0e54e24eb4158
Frank-Colorado <chawnkiee@gmail.com>
2023-07-22 00:35:58 -0400

    created Action Types

---
+++ client/src/context/actionTypes/index.js
@@ -0,0 +1,14 @@
+// These are the action types for the Player List
+export const CREATE_PLAYER_LIST = "CREATE_PLAYER_LIST";
+export const ADD_PLAYER_TO_LIST = "ADD_PLAYER_TO_LIST";
+export const UPDATE_PLAYER_LIST = "UPDATE_PLAYER_LIST";
+export const DELETE_PLAYER_FROM_LIST = "DELETE_PLAYER_FROM_LIST";
+
+// These are the action types for the Favorites List
+export const CREATE_FAVORITES_LIST = "CREATE_FAVORITES_LIST";
+export const ADD_PLAYER_TO_FAVORITES = "ADD_PLAYER_TO_FAVORITES";
+export const REMOVE_PLAYER_FROM_FAVORITES = "REMOVE_PLAYER_FROM_FAVORITES";
+
+// These are the action types for the User Profile
+export const CREATE_USER_PROFILE = "CREATE_USER_PROFILE";
+export const UPDATE_USER_PROFILE = "UPDATE_USER_PROFILE";
