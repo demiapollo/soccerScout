bcb8d4178bfa43e8c8c54cb9ec4a59a3def005dc
Frank-Colorado <chawnkiee@gmail.com>
2023-07-25 20:49:06 -0400

    changed reassign page in auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -29,7 +29,7 @@ class AuthService {
 
   login(idToken) {
     localStorage.setItem("jwtToken", idToken);
-    window.location.assign("/profile");
+    window.location.assign("/");
   }
 
   logout() {
